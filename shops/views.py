64eb6cb239f2c39c5900ff5cfabb4f5c89276f12
unknown <zurikiser@gmail.com>
2024-10-02 17:56:59 -0400

    Initial commit

---
+++ shops/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Markup Values
+
+html_values = {
+  "google_form_link": "https://docs.google.com/forms/d/e/1FAIpQLSeop8N4cvcL6Dj09266ZGXQxBdkdTjhHCM4WqZ3WXfHPhTR7g/viewform",
+"pure_css_cdn":"https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css","list_link": "https://docs.google.com/spreadsheets/d/1x1DgKM5yyP03AdZXcHDuDZowOHTIMt0-xnx3Cm2Wa0Y/edit?usp=sharing"}
+
+seo_values = {
+  "meta_description": "Explore Maryland's family-owned businesses. Add your favorites, join our Think-Tank for updates, suggest features, and support local shopping!", "meta_keywords": "Maryland local businesses, family-owned shops, Maryland shopping, support local, weekend shopping, holiday shopping, small businesses, community shops, Maryland Think-Tank, local gems"
+}
+
+all_values = {**seo_values, **html_values}
+
+# Views
+
+def home(request):
+  return render(request, "myfirst.html", all_values)  
